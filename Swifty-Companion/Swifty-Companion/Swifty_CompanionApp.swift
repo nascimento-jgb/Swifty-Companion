066a1a005fb9103f592b165ad6a72badf617224c
Joao Nascimento <nascimento.joaogb@gmail.com>
2023-08-19 19:50:17 +0300

    prototype of onboarding page done

---
+++ Swifty-Companion/Swifty-Companion/Swifty_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_CompanionApp.swift
+//  Swifty-Companion
+//
+//  Created by Joao Nascimento on 19.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ Swifty-Companion/Swifty-Companion/View/OnboardingView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Swifty-Companion
+//
+//  Created by Joao Nascimento on 19.8.2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @State private var searchBarText: String  = ""
+    
+    var body: some View {
+        ZStack{
+            
+            //42 BACKGROUND ANIMATION HERE
+            
+            VStack(alignment: .center, spacing: 24){
+                
+                //COMPOSED IMAGE/COLOR WITH BACKGROUND
+                Spacer()
+                
+                Text("SPACE HOLDER 42 LOGO")
+                TextField("Type here the student login", text: $searchBarText)
+                    .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    .shadow(radius: 5, x: 0, y: 3)
+                    .padding(20)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .overlay(RoundedRectangle(cornerRadius: 0, style: .continuous)
+                        .stroke(Color.black.opacity(0.7), lineWidth: 2))
+
+                
+                Button{
+                    //Instert the action here
+                } label: {
+                    Label("Search", systemImage: "arrouw.right")
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .shadow(color: .gray, radius: 20, x:0, y: 10)
+                }
+                
+                Spacer()
+            }
+            .padding(50)
+            .background(.regularMaterial)
+            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .shadow(radius: 5, x: 0, y: 3)
+            .shadow(radius: 30, x: 0, y: 30)
+            .padding(20)
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
